d59bf474a06f35b630578e5a555b2764fcf9c0e5
abdelhamed hamed <157960201+abdelhamed-hamed@users.noreply.github.com>
2024-09-18 23:32:23 +0300

    Update todos.component.ts

--- src/components/users/todos/todos.component.ts
+++ src/components/users/todos/todos.component.ts
@@ -8,11 +8,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './todos.component.scss',
 })
 export class TodosComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private title: Title) {}
   allTodos!: Todos[];
   errorMessage?: string;
   itemComplete!: string[];
   ngOnInit(): void {
+    this.title.setTitle('Todos');
     this.getAllTodos();
   }
 
